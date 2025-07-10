2681a59c3675fdd9f47dee171c7ff7f5e1bfefc0
arpitz04 <arpitvarshney04@gmail.com>
2025-07-10 16:25:25 +0530

    make changes

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for frontend requests
+CORS(app)
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
@@ -30,6 +31,6 @@ def analyze():
         "confidence": confidence
     })
 
-
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
